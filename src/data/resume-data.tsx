150cf4e00e002820426c66f80a9fdcfa0ebf4ea9
aakashshankar <aakashshankar217@gmail.com>
2024-10-24 21:08:23 -0400

    lowercase

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -15,7 +15,7 @@ export const RESUME_DATA = {
     email: "aakashshankar217@gmail.com",
     tel: "+15513759829",
     social: [
-      {
+      { 
         name: "GitHub",
         url: "https://github.com/aakashshankar",
         icon: GitHubIcon,
@@ -34,134 +34,114 @@ export const RESUME_DATA = {
   },
   education: [
     {
-      school: "New York University",
-      degree: "Master's degree, Computer Science",
+      school: "new york university",
+      degree: "master's degree, computer science",
       start: "2023",
       end: "2025",
     },
     {
-      school: "Visvesvaraya Technological University",
-      degree: "Bachelor's degree, Computer Science & Engineering",
+      school: "visvesvaraya technological university",
+      degree: "bachelor's degree, computer science",
       start: "2017",
       end: "2021",
     },
   ],
   work: [
     {
-      company: "New York University",
+      company: "new york university",
       link: "https://nyu.edu",
       badges: [],
-      title: "Graduate Teaching Assistant",
+      title: "graduate course assistant",
       start: "2024",
-      end: "Present",
-      description: "TA for CSGY 9163 - Application Security. Tech: C, Python, Django, Kotlin, Kubernetes"
+      end: "present",
+      description: "CA for CSGY 9163 - application security. tech: c, python, django, kotlin, kubernetes"
     },
     {
       company: "[stealth startup]",
       link: "https://spotonix.com",
-      badges: [],
-      title: "Software Engineer Intern",
+      badges: ["Remote"],
+      title: "software engineer intern",
       start: "2024",
       end: "2024",
       description:
-        "Enabled hybrid full text and vector search capabilities on [stealth startup]'s knowledge graph by integrating LanceDB. Tech: Python, Pydantic, OpenAI, RAG, LanceDB",
+        "enabled hybrid full text and vector search capabilities on [stealth startup]'s knowledge graph by integrating lanceDB. tech: python, pydantic, openAI, RAG, lanceDB",
     },
     {
-      company: "Cloudera",
+      company: "cloudera",
       link: "https://cloudera.com",
       badges: [],
-      title: "Software Engineer Intern → Software Engineer II",
+      title: "software engineer intern → software engineer II",
       start: "2021",
       end: "2023",
       description:
-        "Engineered cutting edge features for the Cloudera Data Platform. Tech: Spring, Docker, Kubernetes, Apache YARN, AWS",
+        "engineered cutting edge features for the cloudera data platform. tech: java, spring, docker, kubernetes, apache YARN, aws",
     },
     {
-      company: "VeCrear",
+      company: "[startup]",
       link: "https://vecrear.com/",
-      badges: ["Remote"],
-      title: "Software Engineer Intern",
+      title: "software engineer intern",
+      badges: [],
       start: "2019",
       end: "2020",
       description:
-        "Developed automated data pipelines to generate performance analysis reports. Tech: Python, Pandas, Apache Airflow",
+        "developed automated data pipelines to generate performance analysis reports. tech: python, pandas, apache airflow",
     },
   ],
   skills: [
-    "Java",
-    "Python",
-    "Go",
-    "TypeScript",
-    "Angular",
-    "NextJS",
-    "SvelteKit",
-    "Spring",
-    "Gradle",
-    "Maven",
-    "JUnit",
-    "Mockito",
-    "PostgreSQL",
-    "MongoDB",
-    "LanceDB",
-    "Docker",
-    "Kubernetes (EKS, Openshift)",
-    "AWS",
-    "GitHub Actions",
-    "Vercel",
+    "java",
+    "python",
+    "go",
+    "typescript",
+    "angular",
+    "nextJS",
+    "svelteKit",
+    "spring",
+    "gradle",
+    "maven",
+    "junit",
+    "mockito",
+    "postgreSQL",
+    "mongoDB",
+    "lanceDB",
+    "docker",
+    "kubernetes (eks, openshift)",
+    "aws",
+    "gitHub actions",
+    "vercel",
   ],
   projects: [
     {
-      title: "Speech-To-Text",
-      techStack: ["Spring", "Swing", "OpenAI", "PostgreSQL"],
+      title: "medical healthscibe",
+      techStack: ["java", "spring", "swing", "openAI", "postgreSQL"],
       description:
-        "Created a real-time speech to text transcriber for patient-clinician interactions with AI powered NLP capabilities.",
+        "created a real-time speech to text transcriber for patient-clinician interactions with AI powered NLP capabilities.",
       link: {
         label: "GitHub",
         href: "https://github.com/aakashshankar/speech-to-text",
       },
     },
     {
-      title: "Audio Classifier",
-      techStack: ["PyTorch", "Librosa", "Deep Learning"],
-      description:
-        "A simple audio classifier that recognizes and classifies various categories of urban sounds",
-      link: {
-        label: "Jovian",
-        href: "https://jovian.com/aakashshankar/urbansound8kclassification",
-      },
-    },
-    {
-      title: "SHEMS",
-      techStack: ["NextJS", "TypeScript", "Tailwind CSS", "Vercel", "Java", "Spring Boot", "PostgreSQL", "Heroku", "Supabase"],
-      description:
-          "Created a complete fullstack app using NextJS and Spring Boot to manage and monitor the energy consumption of a household, to exercise my skills in frontend and backend development.",
-      link: {
-          label: "GitHub",
-          href: "https://github.com/aakashshankar/shems"
-      },
-    },
-    {
-      title: "Tap&Go",
-      techStack: ["NextJS", "TypeScript", "Tailwind CSS", "Vercel", "Clerk Auth", "PostgreSQL", "Supabase", "MongoDB Atlas", "RAG", "OpenAI", "LangChain"],
-      description: "An AI-powered app integrated with RAG to provide super personalized travel recommendations. Created this as part of the IvyHacks and MongoDB hackathons.",
+      title: "tap&go",
+      techStack: ["nextJS", "typescript", "tailwind css", "clerk auth", "postgreSQL", "supabase", "mongoDB atlas", "RAG", "openAI"],
+      description: "an ai-powered app integrated with RAG to provide super personalized travel recommendations. created this as part of the ivyhacks and mongoDB hackathons.",
       link: {
           label: "GitHub",
           href: "https://github.com/aakashshankar/ivyhacks_tap_and_go"
       }
     },
     {
-      title: "Terminator",
-      techStack: ["Go", "Anthropic", "OpenAI", "Mistral", "Gemini"],
-      description: "An AI assisted terminal that uses LLMs to provide contextual and personalized assisitance to streamline complex workflows and boost productivity.",
+      title: "terminator",
+      techStack: ["go", "anthropic", "openAI", "mistral", "gemini"],
+      description: "an ai assisted terminal that uses LLMs to translates natural language into shell commands, boosting my productivity.",
       link: {
           label: "GitHub",
           href: "https://github.com/aakashshankar/llm-cli"
       }
     },
     {
-      title: "VexDB (WIP)",
-      techStack: ["Go", "Embedding models", "Vector Search", "Database fundamentals"],
-      description: "My very own lightweight, key value vector database",
+      title: "vexDB (wip)",
+      techStack: ["go", "embedding models", "vector search", "database fundamentals"],
+      description: "my very own lightweight, key value vector database",
       link: {
         label: "GitHub",
         href: "https://github.com/aakashshankar/vexdb"
